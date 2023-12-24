ea3c43a9a26c1dd667d3db5dc0c911d91219e7ab
Bogdan Tolstik <bogdantolstik@gmail.com>
2023-12-24 15:46:02 +0100

    upd

--- homework_test.cpp
+++ homework_test.cpp
@@ -1,29 +1,29 @@
 #include <gtest/gtest.h>
 #include "homework.h"
 
-// TEST(AggresiveTest, Entrance1) {
-//     auto order1 = std::make_shared<LimitOrder>(OrderSide::BUY_OS, 0, 99, 50000);
-//     auto order2 = std::make_shared<LimitOrder>(OrderSide::BUY_OS, 1, 98, 25500);
-//     auto order3 = std::make_shared<LimitOrder>(OrderSide::SELL_OS, 2, 100, 10000);
-//     auto order4 = std::make_shared<LimitOrder>(OrderSide::SELL_OS, 3, 100, 7500);
-//     auto order5 = std::make_shared<LimitOrder>(OrderSide::SELL_OS, 4, 101, 20000);
+TEST(AggresiveTest, Entrance1) {
+    auto order1 = std::make_shared<LimitOrder>(OrderSide::BUY_OS, 0, 99, 50000);
+    auto order2 = std::make_shared<LimitOrder>(OrderSide::BUY_OS, 1, 98, 25500);
+    auto order3 = std::make_shared<LimitOrder>(OrderSide::SELL_OS, 2, 100, 10000);
+    auto order4 = std::make_shared<LimitOrder>(OrderSide::SELL_OS, 3, 100, 7500);
+    auto order5 = std::make_shared<LimitOrder>(OrderSide::SELL_OS, 4, 101, 20000);
 
-//     MatchingSystem system;
-//     for (auto order : {order1, order2, order3, order4, order5}) {
-//         system.AddOrder(order);
-//     }
+    MatchingSystem system;
+    for (auto order : {order1, order2, order3, order4, order5}) {
+        system.AddOrder(order);
+    }
     
-//     auto iceberg_order = std::make_shared<IcebergOrder>(OrderSide::BUY_OS, 5, 100, 10000, 100000);
-//     system.AddOrder(iceberg_order);
+    auto iceberg_order = std::make_shared<IcebergOrder>(OrderSide::BUY_OS, 5, 100, 10000, 100000);
+    system.AddOrder(iceberg_order);
 
-//     EXPECT_EQ(iceberg_order->quantity(), 10000);
-//     EXPECT_EQ(iceberg_order->hidden_full_volume(), 82500);
-//     EXPECT_EQ(order1->quantity(), 50000);
-//     EXPECT_EQ(order2->quantity(), 25500);
-//     EXPECT_EQ(order3->quantity(), 0);
-//     EXPECT_EQ(order4->quantity(), 0);
-//     EXPECT_EQ(order5->quantity(), 20000);
-// }
+    EXPECT_EQ(iceberg_order->quantity(), 10000);
+    EXPECT_EQ(iceberg_order->hidden_full_volume(), 82500);
+    EXPECT_EQ(order1->quantity(), 50000);
+    EXPECT_EQ(order2->quantity(), 25500);
+    EXPECT_EQ(order3->quantity(), 0);
+    EXPECT_EQ(order4->quantity(), 0);
+    EXPECT_EQ(order5->quantity(), 20000);
+}
 
 TEST(PassiveTest, Test1) {
     auto order1 = std::make_shared<LimitOrder>(OrderSide::BUY_OS, 0, 99, 50000);
